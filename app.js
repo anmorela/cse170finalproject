13928db6cc7cc106695f9518afc0a40f42f39851
Maxwell Takeda <mjtakeda@ucsd.edu>
2020-02-20 23:27:28 -0800

    small tweaks

--- app.js
+++ app.js
@@ -48,13 +48,13 @@ app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
 app.get("/addCourse", addCourse.viewAddCourse);
-app.get("/course=:courseName", course.view);
+app.get("/course/=:courseName", course.view);
 app.get("/findTeammates", team.view);
-app.get("/groupPage=:groupName", group.view);
+app.get("/groupPage/=:groupName", group.view);
 app.get('/addComment', add.addComment);
 app.get("/homepage", home.view);
 app.get("/Success", success.viewSuccess);
-app.get('/groupPage=:groupName/addTeammate', addTeamMemberPage.view);
+app.get('/groupPage/=:groupName/addTeammate', addTeamMemberPage.view);
 app.get('/addTeammateMethod', addTeammate.addTeammateMethod);
 app.get('/:courseName/addGroup', addGroup.viewAddGroup);
 app.get("/:courseName/addGroup/Success", success.viewGroupSuccess);
